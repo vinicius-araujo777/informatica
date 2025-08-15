37af226afa4363897cca9db02f97b1335eb3c6d0
lohanesthefanyy2 <lohaneSthe0129@outlook.com>
2025-08-15 12:40:13 -0300

    adicionando atividade

---
+++ nf1 (1).py
@@ -0,0 +1,50 @@
+# questão 1
+# n1 = float(input("digite sua nota: "))
+# n2 = float(input("digite sua nota: "))
+# n3 = float(input("digite sua nota: "))
+# m = (n1+n2+n3)/3
+# print(m)
+# if m >=7:
+#     print("aprovado" )
+# else:
+#     print("reprovado" )
+
+
+# questão 2
+# p1=str(input("digite uma palavra:"))
+# cv=0
+# for i in p1:
+#     if i== "a" or i=="e" or i=="i" or i=="o" or i=="u":
+#      cv+=1
+# print(F"total de vogais:{cv}")
+
+
+# questão 3
+# n1 = int(input("digite um número: "))
+# r = n1 %2
+# if r == 0:
+#     print(f"{n1} é par")
+# else:
+#     print(f"{n1} é impar")
+
+
+# Questão 4
+# for i in range(5):
+#     num = int(input('Digite um número: '))
+#     if i == 0 :
+#         maior = menor = num
+#     elif num > maior :
+#         maior = num
+#     elif num < menor :
+#         menor = num
+# print(f"{maior} é o maior numero")
+# print(f"{menor} é o menor numero")
+
+
+# Questão 5
+# sp = 0
+# c = int(input("digite o valor do produto: (digite 0 para parar)"))
+# while c !=0:
+#     sp += int(input("digite o valor do produto"))
+# while c ==0:
+#     print (sp)
